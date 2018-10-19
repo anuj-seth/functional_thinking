2af08b98da57d6deb76500a3cf87f709b0a11b5a
anuj seth <anujseth76@gmail.com>
2018-10-19 14:00:58 +0530

    more changes

---
+++ code/files/my_indexed_test.clj
@@ -0,0 +1,18 @@
+(ns functional-thinking.my-indexed-test
+  (:require [clojure.test :refer :all]))
+
+; write a function called my-indexed that tags each element
+; of a collection with it's index
+; restriction: map-indexed not allowed
+
+(deftest my-indexed-test
+  (let [my-indexed __]
+    (is (= '([0 1] [1 2] [2 3])
+           (my-indexed [1 2 3])))
+    (is (=  '([0 [:a 1]] [1 [:b 2]])
+            (my-indexed {:a 1 :b 2})))
+    (is (= ()
+           (my-indexed [])))
+    (is (= '([0 0] [1 1])
+           (take 2 (my-indexed (range)))))))
+
